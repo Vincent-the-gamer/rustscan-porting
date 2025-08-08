a0141210f49a90a9736db951e78622c31564caca
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-08-08 17:05:27 +0800

    chore: playground.

--- playground/scanner.ts
+++ playground/scanner.ts
@@ -9,20 +9,12 @@ open({
     path
 })
 
-const result = load({
+load({
     library,
     funcName: "scanner_run",
     retType: DataType.String,
     paramsType: [DataType.String, DataType.U64, DataType.U64, DataType.U64, DataType.U64, DataType.U8],
     paramsValue: ["192.168.1.132", 1, 100, 1000, 10, 3]
 })
-
-try {
-    const ports = JSON.parse(result)
-    console.log("Open ports:", ports)
-} catch (e) {
-    console.error("Failed to parse result:", e)
-    console.log("Raw result:", result)
-}
 
 close(library)
